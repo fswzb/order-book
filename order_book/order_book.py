ef239f5d097f8afc053e09768e08a91fa71e5f2e
Przemysław Kuczyński <przemek@kuczynski.dev>
2020-05-26 17:13:01 +0200

    Iceberg orders

--- order_book/order_book.py
+++ order_book/order_book.py
@@ -1,6 +1,6 @@
 import json
 from collections import deque, defaultdict
-from typing import Deque, DefaultDict, Optional
+from typing import Deque, DefaultDict, Optional, Dict
 
 
 class OrderEntry:
@@ -13,14 +13,30 @@ class OrderEntry:
         self.price: int = order['price']
         self.quantity: int = order['quantity']
 
-        self.peak: int = order.get('peak', None)
-        self.visible_quantity = 0 if self.is_iceberg else self.quantity
+        self.peak: int = order['peak'] if self.is_iceberg else 0
+        self._visible_quantity = 0 if self.is_iceberg else self.quantity
+
+    def _update_visible_quantity(self):
+        if self.is_iceberg and self._visible_quantity == 0:
+            reload = min(self.quantity, self.peak)
+            self._visible_quantity = min(self.quantity, self.peak)
+            self.quantity -= reload
+
+    @property
+    def visible_quantity(self):
+        self._update_visible_quantity()
+        return self._visible_quantity
+
+    @visible_quantity.setter
+    def visible_quantity(self, new_value: int):
+        self._visible_quantity = new_value
+        self._update_visible_quantity()
 
     def to_json(self):
         return {
             'id': self.id,
             'price': self.price,
-            'quantity': self.quantity,
+            'quantity': self.visible_quantity,
         }
 
     def __repr__(self):
@@ -33,21 +49,25 @@ class OrderBook:
         self.buy_orders: DefaultDict[int, Deque[OrderEntry]] = defaultdict(deque)
         self.sell_orders: DefaultDict[int, Deque[OrderEntry]] = defaultdict(deque)
 
+        self.broadcast_queue: Deque[Dict] = deque()
+        self.broadcast_map: Dict[int, Dict] = {}
+
         self.min_sell: Optional[int] = None
         self.max_buy: Optional[int] = None
 
     def process_raw_order(self, order_json):
         new_order = OrderEntry(order_json)
 
-        if new_order.is_iceberg:
-            # raise Exception("Not implemented")
-            print("Iceberg not implemented")
-            pass  # TODO implement
-
         if new_order.is_buy:
-            return self.process_buy_order(new_order)
+            self.process_buy_order(new_order)
         else:
-            return self.process_sell_order(new_order)
+            self.process_sell_order(new_order)
+
+        # Broadcast transaction messages
+        while self.broadcast_queue:
+            msg = self.broadcast_queue.popleft()
+            print(json.dumps(msg))
+        self.broadcast_map = {}
 
     def process_sell_order(self, sell_order: OrderEntry):
         # Fulfill existing orders
@@ -56,18 +76,22 @@ class OrderBook:
 
             while buy_offers:
                 buy_offer = buy_offers[0]
-                if buy_offer.quantity < sell_order.quantity:
-                    OrderBook._broadcast_transaction(buy_offer.id, sell_order.id, self.max_buy, buy_offer.quantity)
+                if buy_offer.visible_quantity <= sell_order.quantity:
+                    self._broadcast_transaction(buy_offer.id, buy_offer.id, sell_order.id, self.max_buy,
+                                                buy_offer.visible_quantity)
 
-                    sell_order.quantity -= buy_offer.quantity
+                    sell_order.quantity -= buy_offer.visible_quantity
+                    buy_offer.visible_quantity = 0
                     buy_offers.popleft()
+                    if buy_offer.visible_quantity > 0:
+                        buy_offers.append(buy_offer)
+                    if sell_order.quantity == 0:
+                        return  # Order fulfilled completely
                 else:
-                    OrderBook._broadcast_transaction(buy_offer.id, sell_order.id, self.max_buy, sell_order.quantity)
+                    self._broadcast_transaction(buy_offer.id, buy_offer.id, sell_order.id, self.max_buy,
+                                                sell_order.quantity)
 
-                    if buy_offer.quantity > sell_order.quantity:
-                        buy_offer.quantity -= sell_order.quantity
-                    else:
-                        buy_offers.popleft()
+                    buy_offer.visible_quantity -= sell_order.quantity
 
                     return  # Order fulfilled completely
 
@@ -85,18 +109,22 @@ class OrderBook:
 
             while sell_offers:
                 sell_offer = sell_offers[0]
-                if sell_offer.quantity < buy_order.quantity:
-                    OrderBook._broadcast_transaction(buy_order.id, sell_offer.id, self.min_sell, sell_offer.quantity)
+                if sell_offer.visible_quantity <= buy_order.quantity:
+                    self._broadcast_transaction(sell_offer.id, buy_order.id, sell_offer.id, self.min_sell,
+                                                sell_offer.visible_quantity)
 
-                    buy_order.quantity -= sell_offer.quantity
+                    buy_order.quantity -= sell_offer.visible_quantity
+                    sell_offer.visible_quantity = 0
                     sell_offers.popleft()
+                    if sell_offer.visible_quantity > 0:
+                        sell_offers.append(sell_offer)
+                    if buy_order.quantity == 0:
+                        return  # Order fulfilled completely
                 else:
-                    OrderBook._broadcast_transaction(buy_order.id, sell_offer.id, self.min_sell, buy_order.quantity)
+                    self._broadcast_transaction(sell_offer.id, buy_order.id, sell_offer.id, self.min_sell,
+                                                buy_order.quantity)
 
-                    if sell_offer.quantity > buy_order.quantity:
-                        sell_offer.quantity -= buy_order.quantity
-                    else:
-                        sell_offers.popleft()
+                    sell_offer.visible_quantity -= buy_order.quantity
 
                     return  # Order fulfilled completely
 
@@ -107,26 +135,25 @@ class OrderBook:
         if not self.max_buy or self.max_buy < buy_order.price:
             self.max_buy = buy_order.price
 
-    @staticmethod
-    def _broadcast_transaction(buy_order_id: int, sell_order_id: int, price: int, quantity: int):
-        info_dict = {
-            "buyOrderId": buy_order_id,
-            "sellOrderId": sell_order_id,
-            "price": price,
-            "quantity": quantity,
-        }
-
-        print(json.dumps(info_dict))
+    def _broadcast_transaction(self, tid: int, buy_order_id: int, sell_order_id: int, price: int, quantity: int):
+        if tid in self.broadcast_map:
+            self.broadcast_map[tid]["quantity"] += quantity
+        else:
+            info_msg_dict = {
+                "buyOrderId": buy_order_id,
+                "sellOrderId": sell_order_id,
+                "price": price,
+                "quantity": quantity,
+            }
+            self.broadcast_queue.append(info_msg_dict)
+            self.broadcast_map[tid] = info_msg_dict
 
     def to_json(self):
         return {
-            "sellOrders": [order.to_json()
-                           for sell_orders in self.sell_orders.values()
-                           for order in list(sell_orders)],
             "buyOrders": [order.to_json()
-                          for buy_orders in self.buy_orders.values()
-                          for order in list(reversed(buy_orders))],
+                          for (_, buy_orders) in sorted(self.buy_orders.items(), reverse=True)
+                          for order in buy_orders],
+            "sellOrders": [order.to_json()
+                           for (_, sell_orders) in sorted(self.sell_orders.items())
+                           for order in list(sell_orders)],
         }
-
-    def __repr__(self):
-        return json.dumps(self.__dict__)
